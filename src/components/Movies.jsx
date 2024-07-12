7d16564073f6e53448ad12592c69484d41b43ac9
ArthurVillela <arthur.villela1@gmail.com>
2024-07-12 16:33:47 +0200

    adding review inputbox

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,12 +1,39 @@
 import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import axios from 'axios'
+import { toast } from 'react-toastify';
 
 const Movie = () => {
 
   const { movieId } = useParams()
   const [movie, setMovie] = useState({})
 
+  const [formData, setFormData] = useState({
+    text: '',
+    ratings: '',
+  })
+  
+  function handleChange(e) {
+    const newFormData = structuredClone(formData)
+    newFormData[e.target.name] = e.target.value
+    setFormData(newFormData)
+  }
+  
+  async function handleSubmit(e) {
+    e.preventDefault()
+    try {
+      const { data } = await axios.post(`http://localhost:8000/api/reviews`, formData)
+      const token = data.token
+
+      localStorage.setItem('token', token)
+      toast.success('Review posted!');
+
+    } catch (err) {
+      toast.error(err.response.data.message);
+      console.log(err.response.data)
+    }
+  }
+  
   useEffect(() => {
     async function fetchMovies() {
       const resp = await fetch(`http://localhost:8000/api/movies/${movieId}`)
@@ -16,11 +43,25 @@ const Movie = () => {
     fetchMovies()
   }, [movieId])
 
-console.log(movieId)
+console.log(movie)
 
   return <div className="section">
   <div className="container">
     <img src={movie.poster}/>
+    <form onSubmit={handleSubmit}>
+        <div className="field">
+          <label className="label">Your Review</label>
+          <div className="control">
+            <input
+              className="input"
+              type="text"
+              name={'email'}
+              onChange={handleChange}
+              value={formData.email}
+            />
+          </div>
+        </div>
+    </form>
   </div>
 </div>
 }
