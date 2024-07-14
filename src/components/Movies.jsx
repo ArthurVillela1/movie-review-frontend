579cfd8403573f9698151c71ae19917ebfc826c9
ArthurVillela <arthur.villela1@gmail.com>
2024-07-14 04:01:29 +0200

    new commit

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,148 +1,219 @@
-import { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
-import axios from 'axios'
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
 import { toast } from 'react-toastify';
 
+// Helper function to parse JWT token
+function parseJwt(token) {
+    try {
+        const base64Url = token.split('.')[1];
+        const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+        const jsonPayload = decodeURIComponent(
+            atob(base64)
+                .split('')
+                .map(c => '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2))
+                .join('')
+        );
+
+        return JSON.parse(jsonPayload);
+    } catch (error) {
+        console.error('Error parsing JWT:', error);
+        return null;
+    }
+}
 
 const Movie = () => {
-
-    const { movieId } = useParams()
-    const [movie, setMovie] = useState(null)
-
+    const { movieId } = useParams();
+    const [movie, setMovie] = useState(null);
+    const [userId, setUserId] = useState(null);
+    const [userReviewed, setUserReviewed] = useState(false);
+    const [editingReviewId, setEditingReviewId] = useState(null); // Track which review is being edited
     const [formData, setFormData] = useState({
         text: '',
         ratings: '',
         movie: Number(movieId),
-    })
+    });
 
-    const [userReviewed, setUserReviewed] = useState(false);
-
-    function parseJwt(token) {
+    // Fetch movie data and check if the user has already reviewed it
+    async function fetchMovieAndCheckReview() {
         try {
-            const base64Url = token.split('.')[1];
-            const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-            const jsonPayload = decodeURIComponent(atob(base64).split('').map(c => {
-                return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-            }).join(''));
+            const response = await axios.get(`http://localhost:8000/api/movies/${movieId}`);
+            const movieData = response.data;
+            setMovie(movieData);
 
-            return JSON.parse(jsonPayload);
-        } catch (error) {
-            console.error('Error parsing JWT:', error);
-            return null;
-        }
-    }
-
-    function handleChange(e) {
-        const newFormData = structuredClone(formData)
-        newFormData[e.target.name] = e.target.value
-        setFormData(newFormData)
-    }
-
-    async function handleSubmit(e) {
-        e.preventDefault();
-
-        try {
-            if (userReviewed) {
-                toast.error('You have already reviewed this movie.');
-                return;
-            }else{
-                if (formData.text === '' || formData.ratings === '') {
-                    toast.error('Fill in ratings and reviews to post');
-                    return;
-                } else if (formData.text.length > 60) {
-                    toast.error('Reviews must be under 60 characters');
-                    return;
-                } else if (formData.ratings > 10 || formData.ratings < 0) {
-                    toast.error('Ratings must be between 0 and 10');
-                    return;
+            const token = localStorage.getItem('token');
+            if (token) {
+                const decodedToken = parseJwt(token);
+                if (decodedToken) {
+                    const userId = decodedToken.sub;
+                    setUserId(userId);
+                    const hasReviewed = movieData.reviews.some(review => review.owner.id === userId);
+                    setUserReviewed(hasReviewed);
+                } else {
+                    console.error('Failed to decode JWT token.');
                 }
-    
-                const response = await axios.post(`http://localhost:8000/api/reviews/`, formData, {
-                    headers: {
-                        Authorization: `Bearer ${localStorage.getItem('token')}`,
-                    },
-                });
-                toast.success('Review posted!');
             }
-
-        } catch (err) {
-            toast.error(err.response.data.message);
-            console.error('Error posting review:', err);
+        } catch (error) {
+            console.error('Error fetching movie:', error);
         }
     }
 
     useEffect(() => {
-        async function fetchMovieAndCheckReview() {
-            try {
-                const response = await axios.get(`http://localhost:8000/api/movies/${movieId}`);
-                const movieData = response.data;
-                setMovie(movieData);
+        fetchMovieAndCheckReview();
+    }, [movieId]);
 
-                const token = localStorage.getItem('token');
-                if (token) {
-                    const decodedToken = parseJwt(token); 
-                    if (decodedToken) {
-                        const userId = decodedToken.sub;
-                        const hasReviewed = movieData.reviews.some(review => review.owner.id === userId);
-                        setUserReviewed(hasReviewed);
-                    } else {
-                        console.error('Failed to decode JWT token.');
-                    }
-                }
-            } catch (error) {
-                console.error('Error fetching movie:', error);
+    // Update form data on change
+    function handleChange(e) {
+        const newFormData = { ...formData };
+        newFormData[e.target.name] = e.target.value;
+        setFormData(newFormData);
+    }
+
+    // Handle new review submission
+    async function handleSubmit(e) {
+        e.preventDefault();
+
+        try {
+            if (formData.text === '' || formData.ratings === '') {
+                toast.error('Fill in ratings and reviews to post');
+                return;
+            } else if (formData.text.length > 100) {
+                toast.error('Reviews must be under 100 characters');
+                return;
+            } else if (formData.ratings > 10 || formData.ratings < 0) {
+                toast.error('Ratings must be between 0 and 10');
+                return;
+            }
+
+            await axios.post(`http://localhost:8000/api/reviews/`, formData, {
+                headers: {
+                    Authorization: `Bearer ${localStorage.getItem('token')}`,
+                },
+            });
+            toast.success('Review posted!');
+            fetchMovieAndCheckReview(); // Reload movie data after submit
+            setEditingReviewId(null);    // Reset editing state after successful post
+        } catch (err) {
+            toast.error(err.response.data.message);
+            console.error('Error posting/reviewing:', err);
+        }
+    }
+
+    // Handle review deletion
+    async function handleDeleteReview(reviewId) {
+        try {
+            await axios.delete(`http://localhost:8000/api/reviews/${reviewId}`, {
+                headers: {
+                    Authorization: `Bearer ${localStorage.getItem('token')}`,
+                },
+            });
+            toast.success('Review deleted!');
+            fetchMovieAndCheckReview();
+        } catch (err) {
+            toast.error('Error deleting review:', err);
+            console.error('Error deleting review:', err);
+        }
+    }
+
+    // Handle review update
+    async function handleEditReview(e) {
+        e.preventDefault();
+    
+        try {
+            if (!formData.text || !formData.ratings || !formData.movie) {
+                toast.error('Please fill in all fields.');
+                return;
+            }
+    
+            // Ensure 'owner' is added to formData
+            formData.owner = userId; // Assuming userId is correctly set in your component
+    
+            const response = await axios.put(`http://localhost:8000/api/reviews/${editingReviewId}`, formData, {
+                headers: {
+                    Authorization: `Bearer ${localStorage.getItem('token')}`,
+                },
+            });
+    
+            toast.success('Review updated!');
+            fetchMovieAndCheckReview();  // Refresh the movie data after update
+            setEditingReviewId(null);    // Reset editing state after successful edit
+        } catch (error) {
+            if (error.response) {
+                console.error('Error updating review:', error.response.data);
+                toast.error('Error updating review: ' + error.response.data.detail);
+            } else {
+                console.error('Error updating review:', error.message);
+                toast.error('Error updating review: ' + error.message);
             }
         }
-        fetchMovieAndCheckReview();
-    }, [movieId, parseJwt]);
+    }
 
-    return <>
-    <div className="container">
-        {movie && <>
-            <img id="postertoreview" className="poster" src={movie.poster} alt={`${movie.title} Poster`} />
-            {movie.reviews && movie.reviews.length > 0 && (
-            <div className="reviews">
-                {movie.reviews.slice(-8).reverse().map((review) => {
-                    return <div key={review.id}>
-                        <ul className='reviewslist'>
-                            <li className='review-item'>
-                                <p><b>{review.owner.username}</b> {review.ratings} {review.text}</p>
-                            </li>
-                        </ul>
-                    </div>
-                })}
-            </div>
+    // Start editing a review
+    const startEditingReview = (review) => {
+        setFormData({
+            text: review.text,
+            ratings: review.ratings,
+            movie: Number(movieId),
+        });
+        setEditingReviewId(review.id); // Set the ID of the review being edited
+    };
+
+    return (
+        <div className="container">
+            {movie && (
+                <>
+                    <img id="postertoreview" className="poster" src={movie.poster} alt={`${movie.title} Poster`} />
+                    {movie.reviews && movie.reviews.length > 0 && (
+                        <div className="reviews">
+                            {movie.reviews.slice(-8).reverse().map((review) => (
+                                <div key={review.id}>
+                                    <ul className='reviewslist'>
+                                        <li className='review-item'>
+                                            {review.owner.id === userId ? (
+                                                <p>
+                                                    <b>{review.owner.username}</b> {review.ratings} {review.text}
+                                                    <button onClick={() => startEditingReview(review)}>Edit</button>
+                                                    <button onClick={() => handleDeleteReview(review.id)}>Delete</button>
+                                                </p>
+                                            ) : (
+                                                <p><b>{review.owner.username}</b> {review.ratings} {review.text}</p>
+                                            )}
+                                        </li>
+                                    </ul>
+                                </div>
+                            ))}
+                        </div>
+                    )}
+                    <form onSubmit={editingReviewId ? handleEditReview : handleSubmit}>
+                        <div className="field">
+                            <div className="control">
+                                <label className="label">Review</label>
+                                <textarea
+                                    id='reviewbox'
+                                    className="input"
+                                    name='text'
+                                    onChange={handleChange}
+                                    value={formData.text}
+                                />
+                            </div>
+                            <div className="control">
+                                <label className="label">Rating</label>
+                                <input
+                                    id='ratingbox'
+                                    className="input"
+                                    type="number"
+                                    name='ratings'
+                                    onChange={handleChange}
+                                    value={formData.ratings}
+                                />
+                            </div>
+                        </div>
+                        <button className="postReviewButton">{editingReviewId ? 'Update' : 'Post'}</button>
+                    </form>
+                </>
             )}
-            <form onSubmit={handleSubmit}>
-                <div className="field">
-                    <div className="control">
-                        <label className="label">Review</label>
-                        <input
-                            id='reviewbox'
-                            className="input"
-                            type="text"
-                            name={'text'}
-                            onChange={handleChange}
-                            value={formData.text}
-                        />
-                    </div>
-                    <div className="control">
-                        <label className="label">Rating</label>
-                        <input
-                            id='ratingbox'
-                            className="input"
-                            type="number"
-                            name={'ratings'}
-                            onChange={handleChange}
-                            value={formData.ratings}
-                        />
-                    </div>
-                </div>
-                <button className="postReviewButton">Post</button>
-            </form>
-        </>}
-    </div>
-</>
-}
+        </div>
+    );
+};
 
-export default Movie
+export default Movie;
